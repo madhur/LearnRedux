1e50c020c1fc0ac68280edc76a8203d7bc5a36a7
Madhur <madhur.ahuja@helpchat.in>
2016-05-29 12:52:40 +0530

    update

--- app/app.jsx
+++ app/app.jsx
@@ -2,11 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
-var Weather = require('./components/Weather.jsx');
-var Examples = require('./components/Examples.jsx');
-var About = require('./components/About.jsx');
-var Main = require('./components/Main.jsx');
-
 
 // Load foundation
 require('style!css!bootstrap/dist/css/bootstrap.min.css')
@@ -14,13 +9,6 @@ require('style!css!bootstrap/dist/css/bootstrap.min.css')
 // App css
 require('style!css!sass!applicationStyles')
 
-ReactDOM.render(
-		<Router history={hashHistory}>
-				<Route path="/" component={Main} >
-					<Route path="about" component={About} />
-					<Route path="examples" component={Examples} />
-					<IndexRoute component={Weather}/>
-				</Route>
-		</Router>
+ReactDOM.render(<div></div>
 	, 
 	document.getElementById('main'));
